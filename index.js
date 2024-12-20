9ff635d71aa320470bfd4719f0f1837252d76cfb
Mitchell <mwurm819@gmail.com>
2024-12-19 21:08:58 -0600

    added initial randomNumber and computer choice functions

--- index.js
+++ index.js
@@ -1,3 +1,16 @@
+/* Create prompt for players 1 
+return variable for player 1 
+create a function named getComputerChoice that returns randomly rock, paper, scissors
+create prompt to enter their choice of rock, paper, or scissors
+return a message that says 'Rock paper scissors shoot, and then returns a message with 
+choices and winner of the round"
+*/
+
+let randomNumber = Math.random() * 10
+
+function getComputerChoice() {
+    return Math.floor(randomNumber);
+}
 
 
-console.log("Hello world!")
+console.log(getComputerChoice());
